be9ea1ace63bd0193d38d8112f0e45840ac3a881
Isaac Morneau <isaacmorneau@gmail.com>
2017-09-19 13:34:57 +0000

    fixed url

--- get.sh
+++ get.sh
@@ -1,5 +1,5 @@
 #where we are pulling from
-BASE_URL="https://raw.github.com/isaacmorneau/7006-final-script/"
+BASE_URL="https://raw.github.com/isaacmorneau/7006-final-script/master/"
 #get the parts to the script
 #file names
 MAIN="menu.sh"
@@ -7,9 +7,9 @@ USERADD="useradd.sh"
 #APACHE="apache.sh"
 
 #grap the files
-wget $BASE_URL$MAIN
-wget $BASE_URL$USERADD
-#wget $BASE_URL$APACHE
+curl $BASE_URL$MAIN > $MAIN
+curl $BASE_URL$USERADD > $USERADD
+#curl $BASE_URL$APACHE
 
 #now you have all the scripts, run the main thing
 chmod 777 $MAIN
