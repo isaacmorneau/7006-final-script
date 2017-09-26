9e06da4b3ee20bea61af9cfcca5e5e88b4711b79
John Agapeyev <jagapeyev@gmail.com>
2017-09-26 14:27:58 -0700

    Replaced file append with comments denoting full config overwrite

--- apache.sh
+++ apache.sh
@@ -25,24 +25,28 @@ chmod 777 /home/$myUser
 chmod 777 /home/$myUser/public_html
 chmod 777 /home/$myUser/public_html/index.html
 
-passwdConf = "
-<Directory /home/$myUser>
-    AllowOverride None
-    AuthUserFile /var/www/html/passwords/foobar
-    AuthGroupFile /dev/null
-    AuthName test
-    AuthType Basic
-    <Limit GET>
-        require valid-user
-        order deny,allow
-        deny from all
-        allow from all
-    </Limit>
-</Directory>
-"
+#TODO add full userdir.conf string to overwrite with
 
-echo "Appending password block to userdir.conf"
-echo $passwdConf >> /etc/httpd/conf.d/userdir.conf
+cp /etc/httpd/conf.d/userdir.conf ~/userdir.conf.bak
+
+#passwdConf = """
+#<Directory /home/$myUser>
+    #AllowOverride None
+    #AuthUserFile /var/www/html/passwords/foobar
+    #AuthGroupFile /dev/null
+    #AuthName test
+    #AuthType Basic
+    #<Limit GET>
+        #require valid-user
+        #order deny,allow
+        #deny from all
+        #allow from all
+    #</Limit>
+#</Directory>
+#"""
+
+#echo "Appending password block to userdir.conf"
+#echo $passwdConf >> /etc/httpd/conf.d/userdir.conf
 
 #Need to comment out default rules that don't require password in userdir.conf
 
