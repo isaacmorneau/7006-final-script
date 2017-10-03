e5da88e7d6f62167fdfa7a6065d9d49d8beca4b0
John Agapeyev <jagapeyev@gmail.com>
2017-10-03 12:39:41 -0700

    Added samba config

--- samba.sh
+++ samba.sh
@@ -1,6 +1,49 @@
 #backup and overwrite the config
 SMB_CONFIG="""
-@TODO add config here
+# See smb.conf.example for a more detailed config file or
+# read the smb.conf manpage.
+# Run 'testparm' to verify the config is correct after
+# you modified it.
+
+[global]
+	workgroup = CST323
+	server string = Samba Server
+	security = user
+
+	passdb backend = tdbsam
+
+	printing = cups
+	printcap name = cups
+	load printers = yes
+	cups options = raw
+
+[NFSHARE]
+	comments = Win32 Share
+	path = /home/$1
+	public = yes
+	writable = yes
+	printable = no
+
+[homes]
+	comment = Home Directories
+	valid users = %S, %D%w%S
+	browseable = No
+	read only = No
+	inherit acls = Yes
+
+[printers]
+	comment = All Printers
+	path = /var/tmp
+	printable = Yes
+	create mask = 0600
+	browseable = No
+
+[print$]
+	comment = Printer Drivers
+	path = /var/lib/samba/drivers
+	write list = root
+	create mask = 0664
+	directory mask = 0775
 """
 cp /etc/samba/smb.conf ~/smb.conf.bak
 echo $SMB_CONFIG > /etc/samba/smb.conf
